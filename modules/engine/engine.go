c98f80aab29e9768ecc6e059e3c7791d540d8de8
TW - IT - Ray Lin <ray.lin@shoalter.com>
2022-09-18 19:53:00 +0800

    feat: Add ```engin.go``` to initialize ```gin``` server

---
+++ modules/engine/engine.go
@@ -0,0 +1,51 @@
+package engine
+
+import (
+	"go-web-template/modules/controller"
+	"go-web-template/modules/middleware"
+	"go-web-template/modules/repository"
+
+	"github.com/gin-gonic/gin"
+)
+
+type IGinManager interface{}
+
+var instance *gin.Engine
+
+type GinManager struct {
+	MySQLGorm         *repository.MySQLGorm
+	UserController    controller.IUserController
+	ContentController controller.IContentController
+	Middleware        middleware.IMiddleware
+}
+
+func (g *GinManager) GetGinEngine() *gin.Engine {
+	if instance == nil {
+		return g.createGin()
+	}
+
+	return instance
+}
+
+func (g *GinManager) createGin() *gin.Engine {
+	if instance == nil {
+		instance = gin.Default()
+	}
+
+	g.MySQLGorm.CreateMySQLConnection()
+
+	userRoute := instance.Group("/user")
+	{
+		userRoute.POST("/login", g.UserController.Login)
+		userRoute.POST("/logout", g.UserController.Logout)
+		userRoute.POST("/register", g.UserController.Register)
+		userRoute.POST("/verify", g.UserController.Verify)
+	}
+
+	contentRoute := instance.Group("/content", g.Middleware.Verify)
+	{
+		contentRoute.GET("/random", g.ContentController.RandomContent)
+	}
+
+	return instance
+}
