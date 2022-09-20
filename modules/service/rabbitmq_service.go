09aa1d454d65f5cad8a198939b817c2e785dd7b4
TW - IT - Ray Lin <ray.lin@shoatler.com>
2022-09-20 15:10:10 +0800

    refactor: Auto format indentation

--- modules/service/rabbitmq_service.go
+++ modules/service/rabbitmq_service.go
@@ -48,7 +48,7 @@ func (rs RabbitMQService) SendMessage(topic string, message string) error {
 		return exception.ErrDeclareRabbitMQExchangerFailed
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	err = channel.PublishWithContext(
@@ -59,7 +59,7 @@ func (rs RabbitMQService) SendMessage(topic string, message string) error {
 		false,
 		amqp091.Publishing{
 			ContentType: "text/plain",
-			Body: []byte(message),
+			Body:        []byte(message),
 		},
 	)
 	if err != nil {
