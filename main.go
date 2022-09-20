0427fbc5296854a08431a4d8e1d510b6075cda24
TW - IT - Ray Lin <ray.lin@shoatler.com>
2022-09-20 18:31:17 +0800

    feat: Implemet graceful shutdown

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	_ "go-web-template/docs"
 	"go-web-template/modules/engine"
-	"go-web-template/modules/orm/mysql"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -25,8 +31,36 @@ func main() {
 	}
 
 	ginEngine := engine.InitGinManager().GetGinEngine()
+	listenPort := 8080
 
-	ginEngine.Run(":8081")
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", listenPort),
+		Handler: ginEngine.Handler(),
+	}
 
-	defer mysql.CloseMySQL()
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Graceful Shutdown
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	log.Println("Shutdown server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		log.Fatal("Server Shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
