8650fa41f58e889c4390d1186764b1b54bc0397b
TW - IT - Ray Lin <ray.lin@shoatler.com>
2022-09-23 17:15:00 +0800

    test: Case "Login with valid email and password"

--- modules/service/test/user_service_test.go
+++ modules/service/test/user_service_test.go
@@ -26,5 +26,30 @@ func TestLogin(t *testing.T) {
 		UserRepository: mockUserRepository,
 	}
 
+	t.Run("Login with valid email and password", func(t *testing.T) {
+		mockUserRepository.On("FindByEmail", registerEmail).
+			Return(
+				model.User{
+					Password: hashedPassword,
+					Email:    registerEmail,
+				},
+				nil,
+			).
+			Once()
 
+		mockPasswordCrypt.On("Verify", hashedPassword, registerPassword).
+			Return(nil).
+			Once()
+
+		mockJwtManager.On("Create").
+			Return(expectedToken, nil).
+			Once()
+
+		token, err := svc.Login(registerEmail, registerPassword)
+		if err != nil {
+			t.Error(err)
+		}
+
+		assert.Equal(t, expectedToken, token, "Output token is equal to mock token")
+	})
 }
